d9e3b10efac6436b4b08e9888c69c4f115055ad4
arun kumar <alekhya0801@gmail.com>
2020-04-13 22:21:24 +0530

    Added feature to compute percentage of doublet

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -x
 #constants
 HEAD=1
+HEAD_HEAD=1
+HEAD_TAIL=2
+TAIL_HEAD=3
 declare -A dict
 declare -A percent
 dict[H]=0
 dict[T]=0
+dict[HH]=0
+dict[HT]=0
+dict[TH]=0
+dict[TT]=0
 read -p "enter number of flips" n
 for (( flip=1; flip<=$n; flip++ ))
 do
@@ -14,9 +21,24 @@ do
 	else
 		((dict[T]++))
 	fi
+	case $((RANDOM%4)) in
+		$HEAD_HEAD)
+			((dict[HH]++))
+		;;
+                $HEAD_TAIL)
+                        ((dict[HT]++))
+                ;;
+                $TAIL_HEAD)
+                        ((dict[TH]++))
+                ;;
+                *)
+                        ((dict[TT]++))
+                ;;
+	esac
 done
 for key in "${!dict[@]}"
 do
 	percent[$key]=$((${dict[$key]}*100/$n))
 done
-echo percentage of heads and tails are ${percent[@]} respectively
+echo number of ${!dict[@]} are ${dict[@]}
+echo percentage of ${!percent[@]} are ${percent[@]} respectively
