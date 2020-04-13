75725938af8688f2b527f199fffcb2c588a8db6d
arun kumar <alekhya0801@gmail.com>
2020-04-13 21:58:41 +0530

    Added feature to compute percentage of singlet combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,22 @@
 #!/bin/bash -x
 #constants
 HEAD=1
-if [ $((RANDOM%2)) -eq $HEAD ]
-then
-	echo Heads
-else
-	echo Tails
-fi
+declare -A dict
+declare -A percent
+dict[H]=0
+dict[T]=0
+read -p "enter number of flips" n
+for (( flip=1; flip<=$n; flip++ ))
+do
+	if [ $((RANDOM%2)) -eq $HEAD ]
+	then
+		((dict[H]++))
+	else
+		((dict[T]++))
+	fi
+done
+for key in "${!dict[@]}"
+do
+	percent[$key]=$((${dict[$key]}*100/$n))
+done
+echo percentage of heads and tails are ${percent[@]} respectively
