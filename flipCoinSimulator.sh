da904ba9822ffe481dcb6aeb68972434d5b03a40
arun kumar <alekhya0801@gmail.com>
2020-04-13 22:53:42 +0530

    Added feature to compute percentage of Triplet combinations

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,14 +4,16 @@ HEAD=1
 HEAD_HEAD=1
 HEAD_TAIL=2
 TAIL_HEAD=3
+H_H_H=1
+H_H_T=2
+H_T_H=3
+H_T_T=4
+T_H_H=5
+T_T_H=6
+T_H_T=7
 declare -A dict
 declare -A percent
-dict[H]=0
-dict[T]=0
-dict[HH]=0
-dict[HT]=0
-dict[TH]=0
-dict[TT]=0
+dict=( [H]=0 [T]=0 [HH]=0 [HT]=0 [TH]=0 [TT]=0 [HHH]=0 [HHT]=0 [HTH]=0 [HTT]=0 [THH]=0 [TTH]=0 [THT]=0 [TTT]=0 )
 read -p "enter number of flips" n
 for (( flip=1; flip<=$n; flip++ ))
 do
@@ -33,6 +35,32 @@ do
                 ;;
                 *)
                         ((dict[TT]++))
+                ;;
+	esac
+	case $((RANDOM%8)) in
+                $H_H_H)
+                        ((dict[HHH]++))
+		;;
+                $H_H_T)
+                        ((dict[HHT]++))
+                ;;
+                $H_T_H)
+                        ((dict[HTH]++))
+                ;;
+                $H_T_T)
+                        ((dict[HTT]++))
+                ;;
+                $T_T_H)
+                        ((dict[TTH]++))
+                ;;
+                $T_H_T)
+                        ((dict[THT]++))
+                ;;
+                $T_H_H)
+                        ((dict[THH]++))
+                ;;
+                *)
+                        ((dict[TTT]++))
                 ;;
 	esac
 done
