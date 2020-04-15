be8a8e780560d9592755e72de0767ba1932bcfaa
arun kumar <alekhya0801@gmail.com>
2020-04-15 12:23:45 +0530

    Refracted code with proper identation

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -5,8 +5,8 @@ HEAD=1
 max=0
 declare -A frequency
 declare -A percentage
-read -p "enter number of coins" numOfCoins
-read -p "enter number of flips" numOfFlips
+read -p "enter number of coins " numOfCoins
+read -p "enter number of flips " numOfFlips
 function findCombination(){
         for (( coin=0; coin<$numOfCoins; coin++ ))
         do
@@ -19,24 +19,28 @@ function findCombination(){
                 fi
                 combination="$combination$toss"
         done
-echo $combination
+	echo $combination
 }
+#compute the frequency of combinations
 for (( flip=0; flip<$numOfFlips; flip++ ))
 do
+	#calling function to find combination
         combinations="$( findCombination )"
-        frequency[$combinations]=$((${frequency[$combinations]}+1))
+	frequency[$combinations]=$((${frequency[$combinations]}+1))
 done
+#compute percentage of combinations and find winning combination
 for key in "${!frequency[@]}"
 do
         percentage[$key]=$((${frequency[$key]}*100/$numOfFlips))
         if [ ${percentage[$key]} -gt $max ]
         then
                 max=${percentage[$key]}
-                winningCombination=$key
+		winningCombination=$key
         fi
 done
+#sort combinations in ascending order by value
 for k in "${!percentage[@]}"
 do
         echo $k ' - ' ${percentage["$k"]}
 done | sort -n -k3
-echo winning combination is $winningCombination with percentage of $max
+echo winning combinations is $winningCombination with percentage of $max
